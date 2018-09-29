d98ea0bacc98270d84ee90aa8d2f210d94460bc3
Athrosus <43689222+Athrosus@users.noreply.github.com>
2018-09-29 03:36:02 +0200

    Added UnityProject
    DreamForge, just added OnPlay: Give a card to your hand; effect

---
+++ RealCardGame/Assets/Scenes/AllCards.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AllCards : MonoBehaviour {
+
+    
+    public List<CardStats> AllCardsObj = new List<CardStats>();
+
+    public void OnButtonPress()
+    {
+
+        
+    AllCardsObj.Add( new CardStats());
+    }
+    
+
+    
+}
---
+++ RealCardGame/Assets/Scenes/DeckList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckList : MonoBehaviour {
+
+    public List<CardStats> CardsInDeck = new List<CardStats>();
+    public void FixedUpdate()
+    {
+    
+    }
+    public void Awake()
+    {
+        if(this.name == "MadeDeck1" || this.name == "MadeDeck2")
+        DontDestroyOnLoad(this);
+        
+    }
+}
